19126dee15249e4998cf8097079619ce5a06790d
sainathreddyseelam <ism2015006@iiita.ac.in>
2024-10-12 23:49:07 +0530

    authentication Addition

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { HashRouter, Route, Routes } from "react-router-dom";
+
+import AuthProvider from "./context/AuthContext";
+import Home from './components/Home'
+import Login from './routes/Login'
+import SignUp from './routes/SignUp'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <HashRouter>
+  <AuthProvider>
+    <Routes>
+      <Route element={<App />}>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<SignUp />} />
+      </Route>
+    </Routes>
+  </AuthProvider>
+  </HashRouter>
   </React.StrictMode>,
 )
